4e1bdcd852427c89978f884fe89bccc3e0ae9bc7
Osdark <o.velasquez@finextechs.com>
2021-07-20 14:00:56 -0500

    added authority on controllers

--- src/main/java/com/zeroim/admin/ports/primary/bill/BillController.java
+++ src/main/java/com/zeroim/admin/ports/primary/bill/BillController.java
@@ -4,6 +4,7 @@ import com.zeroim.admin.requests.bill.BillDTO;
 import com.zeroim.admin.requests.bill.CreateBillDTO;
 import com.zeroim.admin.requests.util.Response;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,15 +12,19 @@ import java.util.UUID;
 
 @RequestMapping("/admin/bill")
 public interface BillController {
+    @PreAuthorize("hasAnyAuthority('ADMIN')")
     @PostMapping
     ResponseEntity<Response<BillDTO>> create(@RequestBody CreateBillDTO createBillDTO);
 
+    @PreAuthorize("hasAnyAuthority('ADMIN')")
     @GetMapping
     ResponseEntity<Response<List<BillDTO>>> getAll();
 
+    @PreAuthorize("hasAnyAuthority('ADMIN')")
     @GetMapping("/getById")
     ResponseEntity<Response<BillDTO>> getById(@RequestParam("id") UUID id);
 
+    @PreAuthorize("hasAnyAuthority('ADMIN')")
     @PutMapping("/pay")
     ResponseEntity<Response<Boolean>> payBill(@RequestParam("id") UUID id);
 }
--- src/main/java/com/zeroim/admin/ports/primary/bill/BillDetailController.java
+++ src/main/java/com/zeroim/admin/ports/primary/bill/BillDetailController.java
@@ -3,6 +3,7 @@ package com.zeroim.admin.ports.primary.bill;
 import com.zeroim.admin.requests.bill.BillDetailDTO;
 import com.zeroim.admin.requests.util.Response;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,6 +12,7 @@ import java.util.UUID;
 
 @RequestMapping("/admin/billDetail")
 public interface BillDetailController {
+    @PreAuthorize("hasAnyAuthority('ADMIN')")
     @GetMapping("/getById")
     ResponseEntity<Response<BillDetailDTO>> getById(@RequestParam("id") UUID id);
 }
